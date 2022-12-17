9b89d274cf11464bd0a92f453a7bcbde17dc991c
Soroush Habibi <soroush.habibi.1382@gmail.com>
2022-12-17 22:55:11 +0330

    setup basic libraries

---
+++ build/index.js
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import express from 'express';
+import { Server } from 'socket.io';
+import http from 'http';
+import path from 'path';
+import { fileURLToPath } from 'url';
+let temp = path.dirname(fileURLToPath(import.meta.url)).split('');
+temp.splice(temp.length - 6);
+const ROOT = temp.join('');
+process.env.ROOT = ROOT;
+const log = console.log;
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+io.on("connection", (socket) => {
+    log("someone connect" + socket.id);
+});
+app.use(express.json());
+app.use(express.static(path.join(process.env.ROOT, 'public')));
+app.use((req, res) => {
+    res.sendFile(path.join(ROOT, 'views/index.html'));
+});
+server.listen(process.env.PORT, () => {
+    log(`server is running on port ${process.env.PORT}`);
+});
